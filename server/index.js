5675c73e7eb0009e8fc19b57017c5e8f3d787c50
evg-zlg <evg.zalogin@gmail.com>
2023-04-03 18:20:55 +0500

    feat: enable all CORS requests

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 // const loadData = require('./data/loadFlatsData');
 const flatRouter = require('./routes/flat.routes');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use(cors());
 app.use(express.json())
 app.use('/', flatRouter);
 // loadData()
--- server/routes/flat.routes.js
+++ server/routes/flat.routes.js
@@ -3,7 +3,7 @@ const router = new Router();
 const flatController = require('../controller/flat.controller');
 
 router.post('/flat', flatController.createFlat);
-router.get('/flat', flatController.getFlats);
+router.get('/flats', flatController.getFlats);
 router.get('/flat/:id', flatController.getOneFlat);
 
 
