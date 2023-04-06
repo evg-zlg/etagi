d28ede18e61e220c030d2aee98c22095cb0d5ef7
evg-zlg <evg.zalogin@gmail.com>
2023-04-06 18:30:44 +0500

    fix: delete useless code

--- client/src/Pages/Main/Main.tsx
+++ client/src/Pages/Main/Main.tsx
@@ -1,25 +1,10 @@
-import styled from 'styled-components';
 import { OneSection } from '../../components/OneSection';
 import { Wrapper } from '../../styles/components/Wrapper';
-
-const Block = styled.div``;
-const El = styled.button`
-  width: 50px;
-  height: 20px;
-  border: 1px solid red;
-  &:hover {
-    border: 1px solid green;
-  }
-`;
 
 function Main() {
   return (
     <Wrapper>
       <OneSection />
-      <Block>
-        <El />
-        <El />
-      </Block>
     </Wrapper>
   );
 }
